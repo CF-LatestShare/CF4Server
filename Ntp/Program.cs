92e81a9c12fe8c5866aee73df5d560d28964da06
DonHitYep <zhengdonn@gmail.com>
2020-10-14 23:11:10 +0800

    Update：新增ntp库；

---
+++ Ntp/Program.cs
@@ -0,0 +1,31 @@
+﻿using GuerrillaNtp;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Ntp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // query the SNTP server
+            TimeSpan offset;
+            try
+            {
+                using (var ntp = new NtpClient(Dns.GetHostAddresses("pool.ntp.org")[0]))
+                    offset = ntp.GetCorrectionOffset();
+            }
+            catch (Exception ex)
+            {
+                // timeout or bad SNTP reply
+                offset = TimeSpan.Zero;
+            }
+
+            // use the offset throughout your app
+            DateTime accurateTime = DateTime.UtcNow + offset;
+            Console.WriteLine(accurateTime);
+            Console.ReadLine();
+        }
+    }
+}
